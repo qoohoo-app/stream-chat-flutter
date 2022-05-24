58d89fd7044ce8986ea822dd93a70025e84bea34
arnav <carnav098@gmail.com>
2022-05-24 11:01:12 +0530

    add is connected method

--- packages/stream_chat_persistence/lib/src/stream_chat_persistence_client.dart
+++ packages/stream_chat_persistence/lib/src/stream_chat_persistence_client.dart
@@ -434,7 +434,9 @@ class DirectDbAccess {
   ) =>
       SharedDB.constructDatabase(userId, connectionMode: mode);
 
-  @override
+  ///Check wheather database is already connected
+  bool isConnected() => db != null;
+
   Future<void> connect(
     String userId,
     ConnectionMode connectionMode,
