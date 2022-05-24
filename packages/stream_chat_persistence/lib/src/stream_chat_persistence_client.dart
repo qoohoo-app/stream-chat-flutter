4db3752c4456448355b9bcb43addee59f04fc3bd
arnav <carnav098@gmail.com>
2022-05-24 10:19:17 +0530

    Move to single file

--- packages/stream_chat_persistence/lib/src/stream_chat_persistence_client.dart
+++ packages/stream_chat_persistence/lib/src/stream_chat_persistence_client.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart' show LogRecord;
 import 'package:mutex/mutex.dart';
 import 'package:stream_chat/stream_chat.dart';
-
 import 'package:stream_chat_persistence/src/db/drift_chat_database.dart';
 
 /// Various connection modes on which [StreamChatPersistenceClient] can work
@@ -53,8 +52,7 @@ class StreamChatPersistenceClient extends ChatPersistenceClient {
     if (record.stackTrace != null) print(record.stackTrace);
   }
 
-  Future<T> _readProtected<T>(AsyncValueGetter<T> func) =>
-      _mutex.protectRead(func);
+  Future<T> _readProtected<T>(AsyncValueGetter<T> func) => _mutex.protectRead(func);
 
   bool get _debugIsConnected {
     assert(() {
@@ -86,8 +84,7 @@ class StreamChatPersistenceClient extends ChatPersistenceClient {
         'disconnect the previous instance before connecting again.',
       );
     }
-    db = databaseProvider?.call(userId, _connectionMode) ??
-        _defaultDatabaseProvider(userId, _connectionMode);
+    db = databaseProvider?.call(userId, _connectionMode) ?? _defaultDatabaseProvider(userId, _connectionMode);
   }
 
   @override
@@ -354,8 +351,7 @@ class StreamChatPersistenceClient extends ChatPersistenceClient {
     assert(_debugIsConnected, '');
     _logger.info('deletePinnedMessageReactionsByMessageId');
     return _readProtected(
-      () =>
-          db!.pinnedMessageReactionDao.deleteReactionsByMessageIds(messageIds),
+      () => db!.pinnedMessageReactionDao.deleteReactionsByMessageIds(messageIds),
     );
   }
 
@@ -389,8 +385,7 @@ class StreamChatPersistenceClient extends ChatPersistenceClient {
   }
 
   @override
-  Future<void> disconnect({bool flush = false}) async =>
-      _mutex.protectWrite(() async {
+  Future<void> disconnect({bool flush = false}) async => _mutex.protectWrite(() async {
         _logger.info('disconnect');
         if (db != null) {
           _logger.info('Disconnecting');
@@ -402,4 +397,74 @@ class StreamChatPersistenceClient extends ChatPersistenceClient {
           db = null;
         }
       });
+}
+
+///Provides direct access to drift DB to query channels in cache and delete channels by CID
+class DirectDbAccess {
+  // ignore: public_member_api_docs
+  DirectDbAccess({
+    Level logLevel = Level.WARNING,
+    LogHandlerFunction? logHandlerFunction,
+  }) : _logger = Logger.detached('💽')..level = logLevel {
+    _logger.onRecord.listen(logHandlerFunction);
+  }
+
+  final Logger _logger;
+  late final ConnectionMode _connectionMode;
+  final _mutex = ReadWriteMutex();
+
+  Future<T> _readProtected<T>(AsyncValueGetter<T> func) => _mutex.protectRead(func);
+
+  @visibleForTesting
+  DriftChatDatabase? db;
+
+  DriftChatDatabase _defaultDatabaseProvider(
+    String userId,
+    ConnectionMode mode,
+  ) =>
+      SharedDB.constructDatabase(userId, connectionMode: mode);
+
+  @override
+  Future<void> connect(
+    String userId,
+    ConnectionMode connectionMode,
+  ) async {
+    if (db != null) {
+      throw Exception(
+        'An instance of StreamChatDatabase is already connected.\n'
+        'disconnect the previous instance before connecting again.',
+      );
+    }
+    _connectionMode = connectionMode;
+    db = _defaultDatabaseProvider(userId, _connectionMode);
+  }
+
+  Future<void> disconnect({bool flush = false}) async => _mutex.protectWrite(() async {
+        if (db != null) {
+          if (flush) {
+            await db!.flush();
+          }
+          await db!.disconnect();
+          db = null;
+        }
+      });
+
+  Future<List<ChannelModel>> getCacheChannels({
+    Filter? filter,
+  }) {
+    _logger.info('get channel in cache');
+    return _readProtected(
+      () async {
+        final channels = await db!.channelQueryDao.getChannels(
+          filter: filter,
+        );
+        return channels;
+      },
+    );
+  }
+
+  Future<void> deleteChannels(List<String> cids) {
+    _logger.info('delete channels in cache from cid');
+    return _readProtected(() => db!.channelDao.deleteChannelByCids(cids));
+  }
 }
--- packages/stream_chat_persistence/lib/stream_chat_persistence.dart
+++ packages/stream_chat_persistence/lib/stream_chat_persistence.dart
@@ -1,4 +1,3 @@
 library stream_chat_persistence;
 
-export 'src/direct_db_access.dart';
 export 'src/stream_chat_persistence_client.dart';
